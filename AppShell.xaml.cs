b5858ac9ccadfa7dfb7ae63b31c7db1901355f1c
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2025-11-08 01:09:14 +0200

    Main page

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChessTrainerApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ChessTrainerApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
